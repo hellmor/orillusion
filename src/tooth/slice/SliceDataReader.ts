4349b0af17a7b9532f7557fe884f3ed53cac3feb
hellmor <dymmky@126.com>
2024-04-04 11:45:02 +0800

    remove isReading

--- src/tooth/slice/SliceDataReader.ts
+++ src/tooth/slice/SliceDataReader.ts
@@ -11,17 +11,12 @@ export class SliceDataReader {
     private _interval: number = 0;
     private _buffer: ComputeGPUBuffer;
     private _index: number;
-    private _isReading: boolean;
     public readonly callback: CDelegate = new CDelegate();
     public get isReading(): boolean {
-        return this._isReading;
+        return this._buffer?.['_readFlag'];
     }
 
     public readBuffer(buffer: ComputeGPUBuffer, index: number) {
-        if (this._isReading) {
-            throw new Error('只能同时读取一次数据！');
-        }
-        this._isReading = true;
         this._buffer = buffer;
         this._index = index;
         this._interval = setInterval(() => { this.checkReadingComplete() }, 1);
@@ -47,8 +42,6 @@ export class SliceDataReader {
             data.array = array;
             data.count = count;
             data.index = this._index;
-
-            this._isReading = false;
             this.callback.execute(data);
         }
     }
